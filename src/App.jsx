16324e73b33cf86bcbb1ea3313d29c44152c01d3
Julian Alejandro Sanchez Burbano <alejo15san95@gmail.com>
2022-02-16 23:20:08 -0500

    Moviendo elemento de un grupo a otro pero sin ordenamiento correcto

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,101 @@
+import { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="item-container">
-          <div className="drop-item">
-            <div className="drag-item"></div>
-          </div>
-          <div className="drop-item">
-            <div className="drag-item"></div>
-          </div>
-          <div className="drop-item">
-            <div className="drag-item"></div>
-          </div>
-        </div>
+const initialData = [
+  { id: "0", name: "item-1", dropId: "A-drop" },
+  { id: "1", name: "item-2", dropId: "A-drop" },
+  { id: "2", name: "item-3", dropId: "A-drop" },
+];
 
-        <div className="item-container">
-          <div className="drop-item"></div>
-          <div className="drop-item"></div>
-          <div className="drop-item"></div>
+function App() {
+  const [items, setItems] = useState(initialData);
+
+  const returnItemsForSlock = (dropId, esInicial, itemID) => {
+    return items
+      .filter((item) =>
+        esInicial
+          ? `${item.dropId}-${item.id}` === dropId && item.id === itemID
+          : `${item.dropId}-${item.id}` === dropId
+      )
+      .map((currenItem) => (
+        <Draggable
+          key={currenItem.id}
+          draggableId={`${currenItem.dropId}-${currenItem.id}`}
+          index={Number(currenItem.id)}
+        >
+          {(draggableProvided) => (
+            <div
+              {...draggableProvided.draggableProps}
+              {...draggableProvided.dragHandleProps}
+              className="drag-item"
+              ref={draggableProvided.innerRef}
+            >
+              {currenItem.name}
+            </div>
+          )}
+        </Draggable>
+      ));
+  };
+
+  const onDragEnd = (result) => {
+    console.log(result);
+    const { draggableId, source } = result;
+    setItems((prev) =>
+      prev.map((item) => {
+        if (`${item.dropId}-${item.id}` === draggableId) {
+          console.log("Item encontrado", item.name);
+          return {
+            ...item,
+            dropId: "B-drop",
+          };
+        }
+        return item;
+      })
+    );
+  };
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="App">
+        <div className="container">
+          <div className="item-container">
+            {items.map((item, index) => (
+              <Droppable key={index} droppableId={`A-drop-${item.id}`}>
+                {(droppableProvided) => (
+                  <div
+                    {...droppableProvided.droppableProps}
+                    className="drop-item"
+                    ref={droppableProvided.innerRef}
+                  >
+                    {returnItemsForSlock(`A-drop-${item.id}`)}
+                    {droppableProvided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            ))}
+          </div>
+
+          <div className="item-container">
+            {items.map((item, index) => (
+              <Droppable key={index} droppableId={`B-drop-${item.id}`}>
+                {(droppableProvided) => (
+                  <div
+                    {...droppableProvided.droppableProps}
+                    className="drop-item"
+                    ref={droppableProvided.innerRef}
+                  >
+                    {returnItemsForSlock(`B-drop-${item.id}`)}
+                    {droppableProvided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </DragDropContext>
   );
 }
 
