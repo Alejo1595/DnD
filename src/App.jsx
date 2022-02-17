a9045ecc405494a5633043ae4a4c2f7b69a6a2fd
Julian Alejandro Sanchez Burbano <alejo15san95@gmail.com>
2022-02-16 22:23:41 -0500

    Template base

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      Prueba
+      <div className="container">
+        <div className="item-container">
+          <div className="drop-item">
+            <div className="drag-item"></div>
+          </div>
+          <div className="drop-item">
+            <div className="drag-item"></div>
+          </div>
+          <div className="drop-item">
+            <div className="drag-item"></div>
+          </div>
+        </div>
+
+        <div className="item-container">
+          <div className="drop-item"></div>
+          <div className="drop-item"></div>
+          <div className="drop-item"></div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
